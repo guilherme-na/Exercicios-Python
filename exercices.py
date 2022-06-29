47c7104443ed59c601cf5e143f9203a5046757f4
Guilherme N. Araújo <58076906+guilherme-na@users.noreply.github.com>
2022-06-28 22:02:12 -0300

    Create exercices.py

---
+++ exercices.py
@@ -0,0 +1,19 @@
+ //Exercicio 01 (If e Else,print)
+
+print("************************************")
+print("Bem Vindo ao Jogo de Adivinhação! :)")
+print("************************************")
+
+num_secreto = 42
+chute = input("Digite o seu numero:" )
+
+print("Você digitou:", chute)
+
+if (num_secreto == chute):
+    print("Você Acertou!!")
+else:
+    print("Você errou!")
+
+
+
+
